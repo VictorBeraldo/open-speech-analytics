4c5c3c0e0b9fad39a92dde7a4968d3bc69bcfed2
Victor Beraldo <VictorBeraldo@users.noreply.github.com>
2024-05-29 00:32:14 -0300

    add rule based model

--- README.md
+++ README.md
@@ -5,11 +5,11 @@
 - [x] 1. Criar uma lista de vídeos.
 - [x] 2. Gerar lista de transcrições Whisper Local com ou sem GPU e por API (Document Loaders).
 - [x] 3. Elaborar lista de resumos com LangChain e LLM (Document Splitting, Summarization).
-- [ ] 4. Implementar categorização multiclasse com categorias pré-definidas, como Renda Fixa, Ações, etc (Zero Shot, Prompt Engeneering).
+- [x] 4. Implementar categorização multiclasse com categorias pré-definidas, como Renda Fixa, Ações, etc (Zero Shot, Prompt Engeneering) e comparar abordagens.
 - [ ] 5. Desenvolver gráfico de série temporal com Streamlit para visualizar as categorias.
 - [x] 6. Implementar banco de dados vetoriais para a base de resumos e estragtégias de busca (VectorDB, Retrieval, SelfQuery).
-- [ ] 7. Desenvolver um chat que permite interação com dados e criação de novas categorias .
-- [ ] 8. Integrar chat com resumos usando Langchain para testar categorias por prompt.
+- [x] 7. Desenvolver um chat que permite interação com dados e criação de novas categorias .
+- [x] 8. Integrar chat com resumos usando Langchain para testar categorias por prompt.
 - [ ] 9. Feedback
      - [ ] a. Implementar funcionalidade para ouvir ligações com transcrição para validar categorias.
 - [ ] 10. Extra: Utilizar Feedback para melhorar o modelo
--- src/utils/document_manager.py
+++ src/utils/document_manager.py
@@ -3,6 +3,9 @@ import ast
 from langchain.schema import Document
 import os
 import shutil
+from langchain_community.document_loaders import DataFrameLoader
+from unidecode import unidecode
+
 
 def save_docs_to_csv(docs, file_path):
     """Salva uma lista de documentos LangChain em um arquivo CSV."""
@@ -47,3 +50,45 @@ def delete_contents(folder_path):
             os.unlink(item_path)
         elif os.path.isdir(item_path):
             shutil.rmtree(item_path)
+            
+
+
+
+def normalizar_string(texto):
+    """
+    Normaliza uma string removendo acentos e caracteres especiais.
+
+    Args:
+        texto (str): O texto a ser normalizado.
+
+    Returns:
+        str: O texto normalizado.
+    """
+    return ''.join(
+        unidecode(char) if not char.isalpha() else char
+        for char in texto
+    )
+
+def carregar_dados(caminho_resumos, caminho_links):
+    """
+    Carrega dados de arquivos CSV, normaliza strings e mescla dataframes.
+
+    Args:
+        caminho_resumos (str): Caminho para o arquivo CSV contendo os resumos.
+        caminho_links (str): Caminho para o arquivo CSV contendo os links.
+
+    Returns:
+        pd.DataFrame: DataFrame mesclado e processado.
+    """
+    df_summary = pd.read_csv(caminho_resumos)
+    df_links = pd.read_csv(caminho_links)
+    
+    df_summary["Source"] = df_summary["Source"].apply(lambda x: os.path.splitext(os.path.basename(x))[0])
+    df_summary['Source'] = df_summary['Source'].apply(normalizar_string)
+    df_links['titulo'] = df_links['titulo'].apply(normalizar_string)
+    
+    df = pd.merge(df_summary, df_links, left_on="Source", right_on="titulo")
+    df = df.dropna(axis=1)
+    df = df.drop('keywords', axis=1)
+    
+    return df
